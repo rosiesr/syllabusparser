78ceebbaeef8b49dd6b32aa1ec46c565fa84451b
Daniel B <danielbolja1029@gmail.com>
2020-12-05 15:14:14 -0500

    v1.0

--- syllabus.py
+++ syllabus.py
@@ -8,7 +8,18 @@ don't ruin this file rosie please
 
 import json
 
-file = open("CS 250 Syllabus.txt", "r")
+f = open("CS 250 Syllabus.txt", "r")
 
 def parse(file):
-    return 0
+    out = {}
+    for line in file:
+        if "—" in str(line):
+            linelist = line.split(" — ")
+            work = linelist[0]
+            date = linelist[1]
+            out.update({work:date[:len(date) - 1]})
+    return json.dumps(out)
+
+if __name__ == "__main__":
+    print(parse(f))
+
