030448c73cb896eae7f5e421b4e0a898b1f79780
Rosie Rothschild <rosierothschild@rosies-mbp.attlocal.net>
2020-12-05 15:37:31 -0500

    made datetime objects

--- syllabus.py
+++ syllabus.py
@@ -5,7 +5,7 @@ author: rad
 
 don't ruin this file rosie please
 """
-
+import datetime as dt
 import json
 
 f = open("CS 250 Syllabus.txt", "r")
@@ -16,9 +16,10 @@ def parse(file):
         if "-" in str(line):
             linelist = line.split(" - ")
             work = linelist[0]
-            date = linelist[1]
-            out.update({work:date[:len(date) - 1]})
-    return json.dumps(out)
+            date = linelist[1][:len(linelist[1]) - 1]
+            datetemp = dt.datetime.strptime(date, "%m/%d/%Y")
+            out.update({work:datetemp})
+    return out
 
 if __name__ == "__main__":
     print(parse(f))
