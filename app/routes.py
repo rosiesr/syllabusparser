ae37ac1ac4799c677fc8128125d96b77804056ef
Rosie Rothschild <rosierothschild@rosies-mbp.attlocal.net>
2020-12-05 17:34:49 -0500

    added file upload

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
+from forms import MyForm
 
-dishes={}
-ingredients=[]
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
