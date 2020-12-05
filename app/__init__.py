56e87fea1c44a320e136514b8ad8229a67a827a3
Rosie Rothschild <rosierothschild@rosies-mbp.attlocal.net>
2020-12-05 17:54:08 -0500

    updated file upload

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-value'
+app.config['UPLOAD_EXTENSIONS'] = ['.pdf', '.txt', '.docx']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort, send_from_directory
 from forms import MyForm
+import os
 
 @app.route('/')
 def index():
@@ -9,6 +10,14 @@ def index():
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
+    filename = uploaded_file.filename
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
