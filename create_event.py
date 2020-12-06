b892e72a5ece7e5ba5868981c4bc95e363b9796f
Alexandra GIORNO <alexandra.giorno@Alexandras-MacBook-Pro.local>
2020-12-05 22:04:48 -0500

    New code and comments

---
+++ create_event.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from cal_setup import get_calendar_service
+from syllabus import work, datestr
+
+#if i am being honest I have no clue what i am doing so what i'm commenting out is from the internet
+#but i am gonna try and create this thing from the google api website luv u team <3 
+def main():
+    event = {
+        'summary': work,
+        'start.date': datestr,
+        'end.date': datestr,
+        'reminders':{
+            'useDefault': True,
+        },
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print 'Event created: %s' % (event.get('htmlLink'))
+
+if __name__ == '__main__':
+   main()
+'''
+def main():
+   # creates one hour event tomorrow 10 AM IST
+   service = get_calendar_service()
+
+   d = datetime.now().date()
+   tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
+   start = tomorrow.isoformat()
+   end = (tomorrow + timedelta(hours=1)).isoformat()
+
+   event_result = service.events().insert(calendarId='primary',
+       body={
+           "summary": 'Automating calendar',
+           "description": 'This is a tutorial example of automating google calendar with python',
+           "start": {"dateTime": start, "timeZone": 'Asia/Kolkata'},
+           "end": {"dateTime": end, "timeZone": 'Asia/Kolkata'},
+       }
+   ).execute()
+
+   print("created event")
+   print("id: ", event_result['id'])
+   print("summary: ", event_result['summary'])
+   print("starts at: ", event_result['start']['dateTime'])
+   print("ends at: ", event_result['end']['dateTime'])
+
+if __name__ == '__main__':
+   main()
+'''
