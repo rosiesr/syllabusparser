8ecd82e6823387e45164e8d1f9dfb6e10f813fcf
agiorno102 <alexandra.giorno@gmail.com>
2020-12-06 11:00:06 -0500

    New gcal.py

--- gcal.py
+++ gcal.py
@@ -1,3 +1,40 @@
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+CREDENTIALS_FILE = 'path_to_file/credentials.json'
+
+def get_calendar_service():
+   creds = None
+   # The file token.pickle stores the user's access and refresh tokens, and is
+   # created automatically when the authorization flow completes for the first
+   # time.
+   if os.path.exists('token.pickle'):
+       with open('token.pickle', 'rb') as token:
+           creds = pickle.load(token)
+   # If there are no (valid) credentials available, let the user log in.
+   if not creds or not creds.valid:
+       if creds and creds.expired and creds.refresh_token:
+           creds.refresh(Request())
+       else:
+           flow = InstalledAppFlow.from_client_secrets_file(
+               CREDENTIALS_FILE, SCOPES)
+           creds = flow.run_local_server(port=0)
+
+       # Save the credentials for the next run
+       with open('token.pickle', 'wb') as token:
+           pickle.dump(creds, token)
+
+   service = build('calendar', 'v3', credentials=creds)
+   return service
+
+'''
 from __future__ import print_function
 import datetime
 import pickle
@@ -49,3 +86,4 @@ def postGcal(work, datestr, service):
     event = service.events().insert(calendarId='primary', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
     return {"body": "success"}
+'''
